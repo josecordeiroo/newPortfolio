cd74da8d606fb050c3438b4af9ec31e1f0c21013
José Cordeiro <zecaxcr@gmail.com>
2023-04-24 18:52:15 -0300

    hero component is done for now

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ function Hero() {
           <Title>Desenvolvedor Fullstack</Title>
           <AboutMe>
             <Line src="./img/line.png" />
-            <Subtitle></Subtitle>
+            <Subtitle>Sobre mim</Subtitle>
           </AboutMe>
           <Desc>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
@@ -48,35 +48,63 @@ const Container = styled.div`
 `;
 
 const Left = styled.div`
-
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
 `;
 
 const Title = styled.h1`
-
+  font-size: 74px;
 `;
 const AboutMe = styled.div`
-
+  display: flex;
+  align-items: center;
+  gap: 10px;
 `;
 
 const Line = styled.img`
-
+  height: 5px;
 `;
 
 const Subtitle = styled.h2`
-
+  color: #8e03ff;
 `;
 const Desc = styled.p`
-
+  font-size: 24px;
+  color: lightgray;
 `;
 const Button = styled.button`
-
+  background-color: #8e03ff;
+  color: white;
+  font-weight: 500;
+  font-size: 18px;
+  width: 150px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
 `;
 const Right = styled.div`
-
+  flex: 3;
+  position: relative;
 `;
 
 const Img = styled.img`
+  width: 800px;
+  height: 800px;
+  object-fit: contain;
+  position: absolute;
+  top: 150px;
+  left: 0;
+  right: 0;
+  margin: auto;
+  animation: animate 2s infinite ease alternate;
 
+  @keyframes animate {
+    to {
+      transform: translateY(50px);
+    }
+  }
 `;
-
-
