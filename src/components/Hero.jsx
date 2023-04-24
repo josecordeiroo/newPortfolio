598d656397f4d11dafa5ef3481aeeabff337f3d0
José Cordeiro <zecaxcr@gmail.com>
2023-04-24 18:37:15 -0300

    update in hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,24 @@ function Hero() {
   return (
     <Section>
       <Navbar />
-      <Container>Hero</Container>
+      <Container>
+        <Left>
+          <Title>Desenvolvedor Fullstack</Title>
+          <AboutMe>
+            <Line src="./img/line.png" />
+            <Subtitle></Subtitle>
+          </AboutMe>
+          <Desc>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
+            fugiat ipsa molestias iusto iste ut dignissimos facilis.
+          </Desc>
+          <Button>Conhecer mais</Button>
+        </Left>
+        <Right>
+          {/* 3d model */}
+          <Img src="./img/moon.png" />
+        </Right>
+      </Container>
     </Section>
   );
 }
@@ -29,3 +46,37 @@ const Container = styled.div`
   display: flex;
   justify-content: space-between;
 `;
+
+const Left = styled.div`
+
+`;
+
+const Title = styled.h1`
+
+`;
+const AboutMe = styled.div`
+
+`;
+
+const Line = styled.img`
+
+`;
+
+const Subtitle = styled.h2`
+
+`;
+const Desc = styled.p`
+
+`;
+const Button = styled.button`
+
+`;
+const Right = styled.div`
+
+`;
+
+const Img = styled.img`
+
+`;
+
+
