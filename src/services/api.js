3c7ad7715280d62c1e27a067e6bcac0f72065d7c
José Cordeiro <zecaxcr@gmail.com>
2023-04-24 12:14:08 -0300

    cleaning

--- src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-const Api = axios.create({ baseURL: "http://localhost:3080/api" });
-export default Api;
-
-//https://josecordeiroo-porfolio.herokuapp.com/api
-//http://localhost:3080/api
-//http://192.168.15.65:3000/api
